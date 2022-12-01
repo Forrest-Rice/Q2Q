cc0acd205bd0b3de8a248c7325682fc7405fdd7a
BY <by15242952083@outlook.com>
2022-12-01 21:48:39 +0800

    vv

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
  * @Author: BY by15242952083@outlook.com
  * @Date: 2022-11-30 16:34:35
  * @LastEditors: BY by15242952083@outlook.com
- * @LastEditTime: 2022-11-30 19:40:22
+ * @LastEditTime: 2022-12-01 21:30:19
  * @FilePath: \Q2Q\src\App.vue
  * @Description:
  * Copyright (c) 2022 by BY email: by15242952083@outlook.com, All Rights Reserved.
 -->
+<script setup lang="ts"></script>
+
 <template>
   <RouterView />
 </template>
